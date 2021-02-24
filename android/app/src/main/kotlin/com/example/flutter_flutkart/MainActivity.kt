b13a48925f04afbe4a1efd303c6c8bd2a46470a0
amit marthak <amit.marthak@bytestechnolab.in>
2021-02-24 16:22:57 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_flutkart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_flutkart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+  debugShowCheckedModeBanner: false,
+  home: SplashScreen(),
+
+));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() { 
+    super.initState();
+    Timer(Duration(seconds: 5),() => print('Splash Done'))
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Stack(
+         fit: StackFit.expand,
+         children: <Widget>[
+           Container(
+             decoration: BoxDecoration(color: Colors.redAccent),    
+           ),
+           Column(
+             children:<Widget>[
+               Expanded(
+                 flex: 2,
+                 child: Container(
+                   child: Column(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: <Widget>[
+                       CircleAvatar(
+                         backgroundColor: Colors.white,
+                         radius: 50,
+                         child: Icon(Icons.shopping_bag,
+                         color: Colors.greenAccent,
+                         size: 50,
+                         ),
+                       ),
+                       Padding(padding: EdgeInsets.only(top:10),
+                       ),
+                       Text("FlutKart", 
+                       style: TextStyle(color: Colors.white, 
+                       fontSize: 24,
+                       fontWeight: FontWeight.bold),
+                       )
+                     ],
+                   ),
+                 ),
+                 ),
+                 Expanded(
+                   flex: 1,
+                   child: Column(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: <Widget>[
+                       CircularProgressIndicator(
+
+                       ),
+                       Padding(
+                         padding: EdgeInsets.only(top: 20),
+                       ),
+                       Text("Online Store \n For Everyone", style: TextStyle(color: Colors.white, fontSize: 18,fontWeight: FontWeight.bold),
+                       )
+                     ],
+                   )
+                   )
+             ],
+           ),
+         ],
+       ),
+    );
+  }
+}
